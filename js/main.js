1e83220350f21ae3ce01336ebe169341e9f2ef0e
Jonatas MS <jonatas202030@gmail.com>
2020-07-31 19:32:00 -0300

    Aula 01 introdução

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let nome = "Jonatas Melo";
+let idade = 29;
+let frase = "Japão é o pior time do mundo!";
+console.log(frase.replace("Japão", "Argentina"));
+console.log(frase.toLocaleLowerCase());
+console.log(frase.toLocaleUpperCase());
+console.log(frase.toLowerCase());
